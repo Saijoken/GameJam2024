a7870a696bccfe250881e0763508ea6f463a4aa1
Saijoken <mohben69800@gmail.com>
2024-09-05 13:57:05 +0200

    Codage base camera + test lancement

---
+++ src/classes/camera.py
@@ -0,0 +1,12 @@
+import pygame
+
+class Camera:
+    position_cam : pygame.Vector2
+    # variable joueur a ajouter
+
+    def __init__(self) -> None:
+        pass
+        
+        
+    def getPosition(self) -> pygame.Vector2:
+        return self.position_cam
