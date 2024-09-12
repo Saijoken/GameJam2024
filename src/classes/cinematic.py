a2be91069f5d00affcbef2932b05d0704917683f
Saijoken <mohben69800@gmail.com>
2024-09-12 13:57:28 +0200

    Credits base

--- src/classes/cinematic.py
+++ src/classes/cinematic.py
@@ -8,21 +8,22 @@ class Cinematic:
     def __init__(self, screen):
         self.screen = screen
 
-    def story_screen(self):
-        story_text = [
-            "Il était une fois, dans un monde voué à l'apocalypse,",
-            "2 héros liés par une pierre.",
-            "Cette pierre magique, traversant les temps et les dimensions,",
-            "permettait à ces deux personnes de temps différents",
-            "de communiquer entre elles.",
-            "Afin d'éviter de faire sombrer le monde qu'ils aiment tant,",
-            "ces deux héros vont devoir collaborer ensemble",
-            "afin de sauver le monde.",
-            "Mais qui sait quelles péripéties ils vont rencontrer...",
-            "Est-ce que cette amitié est vouée à durer ?"
+    def story_screen(self, story_text=None, size=19, skip=True):
+        if story_text is None:
+            story_text = [
+                "Il était une fois, dans un monde voué à l'apocalypse,",
+                "2 héros liés par une pierre.",
+                "Cette pierre magique, traversant les temps et les dimensions,",
+                "permettait à ces deux personnes de temps différents",
+                "de communiquer entre elles.",
+                "Afin d'éviter de faire sombrer le monde qu'ils aiment tant,",
+                "ces deux héros vont devoir collaborer ensemble",
+                "afin de sauver le monde.",
+                "Mais qui sait quelles péripéties ils vont rencontrer...",
+                "Est-ce que cette amitié est vouée à durer ?"
         ]
 
-        font = pygame.font.Font("./assets/fonts/RetroGaming.ttf", 19)
+        font = pygame.font.Font("./assets/fonts/RetroGaming.ttf", size)
         
         # Background
         pygame.draw.rect(self.screen, (100, 100, 100), (self.screen.get_width()/2 - 400, self.screen.get_height() / 2 - 250, 800, 500))
@@ -64,18 +65,38 @@ class Cinematic:
                 text_surface.set_alpha(alphas[i])
                 self.screen.blit(text_surface, (self.screen.get_width() / 2 - text.get_width() / 2, self.screen.get_height()/3 - 100 + (text.get_height() + 10) * i))
 
+            if skip:
             # Render "Press any key" text
-            if all(fade_complete):
-                press_key_text = font.render("Appuyez sur la touche \"Espace\" pour commencer", True, (255, 255, 255))
-                press_key_alpha = (pygame.time.get_ticks() % 1000) // 4 
-                press_key_surface = pygame.Surface(press_key_text.get_size(), pygame.SRCALPHA)
-                press_key_surface.fill((255, 255, 255, 0))
-                press_key_surface.blit(press_key_text, (0, 0))
-                press_key_surface.set_alpha(press_key_alpha)
-                self.screen.blit(press_key_surface, (self.screen.get_width() / 2 - press_key_text.get_width() / 2, self.screen.get_height()/3 + press_key_text.get_height() * len(story_text) + 75))
+                if all(fade_complete):
+                    press_key_text = font.render("Appuyez sur la touche \"Espace\" pour commencer", True, (255, 255, 255))
+                    press_key_alpha = (pygame.time.get_ticks() % 1000) // 4 
+                    press_key_surface = pygame.Surface(press_key_text.get_size(), pygame.SRCALPHA)
+                    press_key_surface.fill((255, 255, 255, 0))
+                    press_key_surface.blit(press_key_text, (0, 0))
+                    press_key_surface.set_alpha(press_key_alpha)
+                    self.screen.blit(press_key_surface, (self.screen.get_width() / 2 - press_key_text.get_width() / 2, self.screen.get_height()/3 + press_key_text.get_height() * len(story_text) + 75))
 
             pygame.display.flip()
             clock.tick(60)  # Limit to 60 FPS
-
+    
     def end_screen(self):
-        pass
+        credits_text = [
+            "CREDITS :",
+            "",
+            "",
+            "Game Design :",
+            "Nacer Berkane",
+            "",
+            "Game Development :",
+            "Mohammed Ben Aicha",
+            "Marie Dos Santos",
+            "",
+            "Graphic Design/Level Design :",
+            "Angele Gioanni",
+            "",
+            "Networking :",
+            "Axelle Broyer",
+            ""
+        ]
+
+        self.story_screen(credits_text, 16, )
